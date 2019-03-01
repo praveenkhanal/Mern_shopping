06256fbc528076a288bc4f16c841804ff03adb21
praveenkhanal <praveen_fren@hotmail.com>
2019-03-01 13:30:15 -0800

    action from reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
