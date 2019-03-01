04892f1f5d9726a40dfcf4ad475065579c83b930
praveenkhanal <praveen_fren@hotmail.com>
2019-03-01 12:26:38 -0800

    components to client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import './App.css';
+import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
